d135670aacea6637d249e2fc41cd5618c76acb85
Gerald Condon <gerald@geraldcondon.com>
2017-04-09 20:47:22 +0000

    updating logger

--- nuraljs_micro_logger.js
+++ nuraljs_micro_logger.js
@@ -1,1 +1,29 @@
 module.exports.log=log;
+module.exports.init=init;
+
+var lcd=0;
+var lcd_module;
+var err=null;
+
+function init(options,cb){
+    if(options){
+        if(options.lcd){
+            lcd=1;
+            lcd_module=options.lcd;
+        }
+        cb(err,"Ready");
+    }
+    else{
+        cb(err,"Ready");
+    }
+}
+
+function log(message){
+    console.log(message);
+    if(lcd){
+        lcd_module.log(message);
+    };
+}
+
+
+    
